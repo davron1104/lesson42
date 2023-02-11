f61ccd129e9318728d54a00bda09e0c931e9cde2
Davron Gaziev <davron1104@yandex.com>
2023-02-11 22:29:49 +0500

    second

--- js/script.js
+++ js/script.js
@@ -19,3 +19,26 @@ hearts.forEach(item => {
 
 const oneHeart = document.querySelector('#box');
 console.log(oneHeart);
+
+
+
+// const box = document.getElementById('box');
+// console.log(box);
+
+// // const btns = document.getElementsByTagName('button')[1];
+// const btns = document.getElementsByTagName('button');
+// console.log(btns[1]);
+
+// const circles = document.getElementsByClassName('circle');
+// console.log(circles);
+
+// const hearts = document.querySelectorAll('.heart');
+
+// console.log(hearts);
+
+// hearts.forEach(item => {
+//     console.log(item);
+// });
+
+// const oneHeart = document.querySelector('.heart');
+// console.log(oneHeart);
